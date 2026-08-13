01ecdf5ccf1058376d1f7694f7cde0bc38d531c0
Brendan Lieu <88119024+brelieu05@users.noreply.github.com>
2026-08-12 21:53:50 -0400

    Add: find-the-difference - submission-2

---
+++ Data Structures & Algorithms/find-the-difference/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count_s = Counter(s)
+        count_t = Counter(t)
+
+        for c in count_t:
+            if c not in count_s or count_s[c] < count_t[c]:
+                return c
+        
+            
