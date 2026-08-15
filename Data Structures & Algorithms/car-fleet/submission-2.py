f539476d21a2e39588b5d82744c9c68dac236725
Brendan Lieu <88119024+brelieu05@users.noreply.github.com>
2026-08-15 16:53:01 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+
+        # loop through
+            # calculate time
+            # append time to stack
+            # while stack is not monotonically decreasing
+                # pop
+
+        for p, s in pair:
+            time = ((target - p) / s)
+
+
+            stack.append(time)
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
